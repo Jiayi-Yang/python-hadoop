766c29999e19c633e7eee4f7488ebc3816acc95a
Jiayi <22630327+Jiayi-Yang@users.noreply.github.com>
2020-08-30 14:29:52 -0400

    add stack implementation

---
+++ stack.py
@@ -0,0 +1,26 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def empty(self):
+        return self.items == []
+
+    def push(self, ele):
+        return self.items.append(ele)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+
+s = Stack()
+print(s.empty())
+s.push(0)
+s.push(1)
+s.push(2)
+print(s.peek())
+print(s.pop())
+print(s.peek())
+
