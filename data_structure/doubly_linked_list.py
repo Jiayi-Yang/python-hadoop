3ba9458b7ba4ad12fa76ebedad305c5c5d8a8331
Jaye <22630327+Jiayi-Yang@users.noreply.github.com>
2021-08-18 14:20:16 -0400

    Update and rename doubly_linklist.py to doubly_linked_list.py

--- data_structure/doubly_linked_list.py
+++ data_structure/doubly_linked_list.py
@@ -1,12 +1,12 @@
-class DoublyLinkListNode:
+class DoublyLinkedListNode:
     def __init__(self,value):
         self.value = value
         self.next = None
         self.pre = None
         
-a = DoublyLinkListNode(1)
-b = DoublyLinkListNode(2)
-c = DoublyLinkListNode(3)
+a = DoublyLinkedListNode(1)
+b = DoublyLinkedListNode(2)
+c = DoublyLinkedListNode(3)
 a.next = b
 b.pre = a
 
